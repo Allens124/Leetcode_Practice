8dcd5a71ee82a5e596da08c269d79982442613a0
Allens124 <124allens@gmail.com>
2025-01-07 03:03:57 -0500

    prompting the user to enter the values of the array

--- Algorithms/twoPointers.cpp
+++ Algorithms/twoPointers.cpp
@@ -11,5 +11,10 @@ int main()
         cin >> n;
     }
     int arr[n];
+    cout << "Enter the values of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
     return 0;
 }
