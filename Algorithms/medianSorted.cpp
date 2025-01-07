1bb2245f04e259d314cc3a553b6b815bf4836580
Allens124 <124allens@gmail.com>
2025-01-07 03:23:14 -0500

    handling exceptions for when n is invalid

--- Algorithms/medianSorted.cpp
+++ Algorithms/medianSorted.cpp
@@ -5,5 +5,10 @@ int main()
     int n;
     cout << "Enter the size of an array: ";
     cin >> n;
+    while (n < 1)
+    {
+        cout << "Invalid input! Try again: ";
+        cin >> n;
+    }
     return 0;
 }
