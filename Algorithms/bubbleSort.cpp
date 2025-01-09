08408d9ec5946cb85adb0c0ed4f25f88eda13f2b
Allens124 <124allens@gmail.com>
2025-01-08 19:42:21 -0500

    prompting the user to enter the values of the array

--- Algorithms/bubbleSort.cpp
+++ Algorithms/bubbleSort.cpp
@@ -33,5 +33,10 @@ int main()
         cin >> size;
     }
     int arr[size];
+    cout << "Enter the values of the array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
     return 0;
 }
