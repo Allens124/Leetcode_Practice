04abb27a144bf55f03ce34b26b96d8225a9dc15c
Allens124 <124allens@gmail.com>
2025-01-08 19:37:48 -0500

    while iterating through the array check whether any elements are out of order

--- Algorithms/bubbleSort.cpp
+++ Algorithms/bubbleSort.cpp
@@ -7,6 +7,16 @@ void bubbleSort(int arr[], int size)
     while (swap)
     {
         swap  = false;
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                int temp = arr[i+1];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swap = true;
+            }
+        }
     }
 }
 
