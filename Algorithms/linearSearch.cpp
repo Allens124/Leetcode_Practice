f52695ed190be54943ae94b40e87924137ceaf32
Allens124 <124allens@gmail.com>
2025-01-09 19:24:51 -0500

    added test cases to the main() function

--- Algorithms/linearSearch.cpp
+++ Algorithms/linearSearch.cpp
@@ -15,6 +15,9 @@ int linearSearch(int arr[], int size, int target)
 
 int main()
 {
-    //code;
+    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    cout << linearSearch(arr, 10, 5) << endl;
+    cout << linearSearch(arr, 10, 1) << endl;
+    cout << linearSearch(arr, 10, 100) << endl;
     return 0;
 }
