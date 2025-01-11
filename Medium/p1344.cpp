bc0ec53c8e2dc84d019a7b32dfa72bfe4d8667cc
Allens124 <124allens@gmail.com>
2025-01-11 17:16:46 -0500

    returned the angle between the hour and the minute hands using the ternary operator and the abs() function

--- Medium/p1344.cpp
+++ Medium/p1344.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 double angleClock(int hour, int minutes)
 {
-    double hourHand = (hour%12 + minutes/60.0)*30;
-    double minuteHand = ((60-minutes)%30)*6;
+    double hourHand = hour < 6 ? (hour+minutes/60.0)*30 : ((12-hour)+minutes/60.0)*30;
+    double minuteHand = minutes < 30 ? minutes*6 : (60-minutes)*6;
+    return abs(hourHand-minuteHand);
 }
 
 int main()
