cb126ddc853109dfc1153edbd31c513db5440aef
Allens124 <124allens@gmail.com>
2025-01-10 23:00:33 -0500

    added return statements in the binarySearch() function

--- Algorithms/binarySearch.cpp
+++ Algorithms/binarySearch.cpp
@@ -17,11 +17,11 @@ int binarySearch(int arr[], int low, int high, int target)
     int middle = (low+high)/2;
     if (arr[middle] < target)
     {
-        binarySearch(arr, middle, high, target);
+        return binarySearch(arr, middle, high, target);
     }
     if (arr[middle] > target)
     {
-        binarySearch(arr, low, middle, target);
+        return binarySearch(arr, low, middle, target);
     }
     return middle;
 }
@@ -29,8 +29,8 @@ int binarySearch(int arr[], int low, int high, int target)
 int main()
 {
     int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    cout << binarySearch(arr, 0, 10, 5) << endl;
-    cout << binarySearch(arr, 0, 10, 1) << endl;
-    cout << binarySearch(arr, 0, 10, 100) << endl;
+    cout << binarySearch(arr, 0, 9, 5) << endl;
+    cout << binarySearch(arr, 0, 9, 1) << endl;
+    cout << binarySearch(arr, 0, 9, 100) << endl;
     return 0;
 }
