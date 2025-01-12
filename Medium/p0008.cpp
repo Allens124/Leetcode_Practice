c34f21513795322570e0f4751de9e33a33786f89
Allens124 <124allens@gmail.com>
2025-01-12 16:03:18 -0500

    added test cases in the main() function

--- Medium/p0008.cpp
+++ Medium/p0008.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 int myAtoi(string s)
 {
-    
+
 }
 
 int main()
 {
-    //code;
+    cout << myAtoi("42") << endl;
+    cout << myAtoi(" -042") << endl;
+    cout << myAtoi("1337c0d3") << endl;
+    cout << myAtoi("0-1") << endl;
+    cout << myAtoi("words and 987") << endl;
     return 0;
 }
