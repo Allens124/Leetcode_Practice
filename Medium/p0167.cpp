a9ac069b6793daa403ca836699e0b22cf0e95864
Allens124 <124allens@gmail.com>
2025-01-13 19:40:15 -0500

    renamed p1 and p2 to i1 and i2

--- Medium/p0167.cpp
+++ Medium/p0167.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 vector <int> twoSum(vector <int> numbers, int target)
 {
-    int p1 = 0, p2 = numbers.size()-1;
+    int i1 = 0, i2 = numbers.size()-1;
+    while (i1 < i2)
+    {
+
+    }
 }
 
 int main()
