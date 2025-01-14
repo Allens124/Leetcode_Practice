3345abb0575eb584180a51ac5b97c6ce9dba80e4
Allens124 <124allens@gmail.com>
2025-01-13 19:42:10 -0500

    using the two pointers algorithm, determined the indices that guarantee the target will be reached

--- Medium/p0167.cpp
+++ Medium/p0167.cpp
@@ -7,7 +7,18 @@ vector <int> twoSum(vector <int> numbers, int target)
     int i1 = 0, i2 = numbers.size()-1;
     while (i1 < i2)
     {
-
+        if (numbers[i1]+numbers[i2] < target)
+        {
+            i1++;
+        }
+        else if (numbers[i1]+numbers[i2] > target)
+        {
+            i2--;
+        }
+        else
+        {
+            break;
+        }
     }
 }
 
