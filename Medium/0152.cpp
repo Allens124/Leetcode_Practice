e99320853682500375b09d51b33f7848ba1a7c14
Allens124 <124allens@gmail.com>
2025-01-15 10:33:07 -0500

    determined the largest product using a for loop

--- Medium/0152.cpp
+++ Medium/0152.cpp
@@ -6,8 +6,23 @@ int maxProduct(int nums[], int size)
     int tempProd = nums[0], maxProd = nums[0];
     for (int i = 0; i < size-1; i++)
     {
-
+        if (tempProd*nums[i+1] > tempProd)
+        {
+            tempProd *= nums[i+1];
+        }
+        else if (tempProd*nums[i+1] < tempProd && i < size-2)
+        {
+            if (tempProd*nums[i+1]*nums[i+2])
+            {
+                tempProd *= nums[i+1];
+            }
+        }
+        if (maxProd < tempProd)
+        {
+            maxProd = tempProd;
+        }
     }
+    return maxProd;
 }
 
 int main()
