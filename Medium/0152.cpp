615fbd1583f2644f8f6d5866a7efa43ebaa6dcb4
Allens124 <124allens@gmail.com>
2025-01-15 10:26:38 -0500

    added test cases to the main() function

--- Medium/0152.cpp
+++ Medium/0152.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int maxProduct(int nums[], int size)
 {
-    
+
 }
 
 int main()
 {
-    //code;
+    int nums1[4] = {2, 3, -2, 4};
+    cout << maxProduct(nums1, 4) << endl;
+    int nums2[3] = {-2, 0, -1};
+    cout << maxProduct(nums2, 3) << endl;
     return 0;
 }
