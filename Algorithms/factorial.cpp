e04bfba0742003e3c5e0e841572bd1a25862c99e
Allens124 <124allens@gmail.com>
2025-01-21 20:39:01 -0500

    changed the size of the memo[] array to account for the maximum integer size

--- Algorithms/factorial.cpp
+++ Algorithms/factorial.cpp
@@ -3,28 +3,21 @@ using namespace std;
 
 int factorial(int n)
 {
-    static int memo[35];
-    cout << memo[15] << endl;
-    if (n < 1)
+    static int memo[12];
+    if (n < 2)
     {
-        memo[n] = n;
+        memo[n] = 1;
         return memo[n];
     }
     if (memo[n] == 0)
     {
-        memo[n] = n*factorial[n-1];
+        memo[n] = n*factorial(n-1);
     }
     return memo[n];
 }
 
 int main()
 {
-    int n = 1;
-    for (int i = 1; i < 15; i++)
-    {
-        cout << i << ": " << n*i << endl;
-        n *= i;
-    }
-    cout << factorial(15) << endl;
+    
     return 0;
 }
