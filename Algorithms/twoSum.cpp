c4b5c5e9ee39c5c6399e8bd690222e93f48b8fb1
Allens124 <124allens@gmail.com>
2025-01-23 19:50:43 -0500

    using the two pointers algorithm to search for a zero pair of integers in the arr[] array

--- Algorithms/twoSum.cpp
+++ Algorithms/twoSum.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int twoSumZero(int arr[], int size)
+void twoSumZero(int arr[], int size)
 {
     int i1 = 0, i2 = 0;
-
+    while (i1 < i2)
+    {
+        if (arr[i1]+arr[i2] < 0)
+        {
+            i1++;
+        }
+        else if (arr[i1]+arr[i2] > 0)
+        {
+            i2--;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (i1 == i2)
+    {
+        cout << "No such values." << endl;
+    }
+    else
+    {
+        cout << i1 << " and " << i2 << "." << endl;
+    }
 }
 
 int main()
