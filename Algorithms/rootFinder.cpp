a076a63ad30d301d082403ee9aec5b80614f7c2e
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-05-20 22:24:24 -0400

    added a function, f, to the title line

--- Algorithms/rootFinder.cpp
+++ Algorithms/rootFinder.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 typedef double (*FUNC)(double);
 
-double root(double start, double end, double accuracy = 0.01)
+double root(double start, double end, FUNC f, double accuracy = 0.01)
 {
     int guess = (start+end)/2;
+
     return guess;
 }
 
