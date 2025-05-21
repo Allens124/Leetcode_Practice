0a836db652237f416a9f7fe9317bb778f697c032
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-05-20 22:27:35 -0400

    filled in the root() function with recursive calls

--- Algorithms/rootFinder.cpp
+++ Algorithms/rootFinder.cpp
@@ -6,7 +6,22 @@ typedef double (*FUNC)(double);
 double root(double start, double end, FUNC f, double accuracy = 0.01)
 {
     int guess = (start+end)/2;
-
+    if (f(guess) < -accuracy)
+    {
+        if (f(start) < f(end))
+        {
+            return root(start, guess, f, accuracy);
+        }
+        return root(guess, end, f, accuracy);
+    }
+    if (f(guess) > accuracy)
+    {
+        if (f(start) < f(end))
+        {
+            return root(start, guess, f, accuracy);
+        }
+        return root(guess, end, f, accuracy);
+    }
     return guess;
 }
 
