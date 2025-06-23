441550c52a7cb0ea61252e3420749278eef10d72
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-06-23 10:15:18 -0400

    established a base case for when num is an integer

--- Algorithms/continuedFraction.cpp
+++ Algorithms/continuedFraction.cpp
@@ -7,6 +7,10 @@ vector <int> continuedFraction(double num)
     vector <int> frac;
     int fracPart = num - (int)num;
     frac.push_back(num - fracPart);
+    if (fracPart == 0 || frac.size() == 10)
+    {
+        return frac;
+    }
 }
 
 int main()
