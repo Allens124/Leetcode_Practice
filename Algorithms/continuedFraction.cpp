6d8c8e761ed4005adee0217c56b760d33de2f823
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-06-23 10:56:53 -0400

    fixed error in the continuedFraction() function

--- Algorithms/continuedFraction.cpp
+++ Algorithms/continuedFraction.cpp
@@ -5,12 +5,13 @@ using namespace std;
 vector <int> continuedFraction(double num)
 {
     vector <int> frac;
-    int fracPart = num - (int)num;
+    double fracPart = num - (int)num;
     frac.push_back(num - fracPart);
     if (fracPart == 0 || frac.size() == 10)
     {
         return frac;
     }
+    cout << "hi" << endl;
     return continuedFraction(1/fracPart);
 }
 
