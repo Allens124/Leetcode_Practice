9f83e01bc9784d8827b37e5b380b5ba03ed5a230
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-06-23 10:13:17 -0400

    pushing the integer part of num onto the frac vector

--- Algorithms/continuedFraction.cpp
+++ Algorithms/continuedFraction.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 vector <int> continuedFraction(double num)
 {
-    
+    vector <int> frac;
+    int fracPart = num - (int)num;
+    frac.push_back(num - fracPart);
 }
 
 int main()
