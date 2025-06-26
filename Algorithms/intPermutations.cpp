1e7d80ba40dc60b2de07f7fb63924f5d2859735a
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-06-26 09:41:48 -0400

    handling exceptions for when size is 0 or less

--- Algorithms/intPermutations.cpp
+++ Algorithms/intPermutations.cpp
@@ -10,7 +10,12 @@ vector <vector <int>> permute (int size)
 int main()
 {
     int size;
-    cout << "Enter the number of elements to permute: ";
+    cout << "Enter the number of elements from to permute: ";
     cin >> size;
+    while (size < 1)
+    {
+        cout << "Invalid input! Try again: ";
+        cin >> size;
+    }
     return 0;
 }
