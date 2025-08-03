0cc7e5e19a13e4effc9d1aed5c159045e33f6e80
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-08-03 10:00:21 -0400

    fixed printing condition

--- Data Structures/Heap.cpp
+++ Data Structures/Heap.cpp
@@ -49,7 +49,7 @@ class Heap
         {
             for (int i = 0; i < size; i++)
             {
-                cout << d << endl;
+                cout << data[i] << endl;
             }
         }
     private:
