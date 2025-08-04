2d51f68f084adf25435408cb9b5710236e08cf17
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-08-04 08:59:50 -0400

    created helper methods

--- Algorithms/heapSort.cpp
+++ Algorithms/heapSort.cpp
@@ -1,18 +1,72 @@
 #include <iostream>
 using namespace std;
 
-void heapSort(int array[], int size)
+template <typename T>
+
+void swapData(T heap[], int i, int j)
 {
-    int heap[size];
+    T temp = heap[i];
+    heap[i] = heap[j];
+    heap[j] = temp;
+}
+
+void bubbleUp(T heap[], int i)
+{
+    if (i == 0)
+    {
+        return;
+    }
+    T dn = heap[i];
+    T dp = heap[(i-1)/2];
+    if (dp < dn)
+    {
+        swapData(heap, i, (i-1)/2);
+        bubbleUp(heap, (i-1)/2);
+    }
+}
+
+void bubbleDown(T heap[], int i, int size)
+{
+    if (2*i+1 >= size)
+    {
+        return;
+    }
+    T dn = heap[i];
+    T dl = heap[2*i+1];
+    T dr = dl;
+    if (2*i+2 < size)
+    {
+        dr = heap[2*i+2];
+    }
+    if (dn < dl && dn < dr)
+    {
+        return;
+    }
+    if (dl <= dr)
+    {
+        swapData(i, 2*i+1);
+        bubbleDown(2*i+1);
+    }
+    else
+    {
+        swapData(i, 2*i+2);
+        bubbleDown(2*i+2);
+    }
+}
+
+void heapSort(T array[], int size)
+{
+    T heap[size];
     for (int i = 0; i < size; i++)
     {
         heap[i] = array[i];
-        // add()
+        bubbleUp(array, i);
     }
     for (int i = 0; i < size; i++)
     {
         array[i] = heap[0];
-        // removeMin()
+        swapData(heap, 0, size-1-i);
+        bubbleDown(heap, 0, i+1);
     }
 }
 
