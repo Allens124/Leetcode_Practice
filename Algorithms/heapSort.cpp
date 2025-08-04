f72091582e457e2103a8f9099855f1d2f0301cfb
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-08-03 21:45:23 -0400

    outlined the heapSort() function

--- Algorithms/heapSort.cpp
+++ Algorithms/heapSort.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
-
+void heapSort(int array[], int size)
+{
+    int heap[size];
+    for (int i = 0; i < size; i++)
+    {
+        heap[i] = array[i];
+        // add()
+    }
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = heap[0];
+        // removeMin()
+    }
+}
 
 int main()
 {
