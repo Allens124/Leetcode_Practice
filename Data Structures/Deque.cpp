a46814ba1bd6507e311fc132e3cb934278bc4630
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-08-04 23:31:09 -0400

    added the push_back() and pop_back() methods

--- Data Structures/Deque.cpp
+++ Data Structures/Deque.cpp
@@ -19,19 +19,39 @@ class Deque
         }
         void push_front(T d)
         {
-
+            if (size == 1000)
+            {
+                cout << "Deque overflow." << endl;
+                return;
+            }
+            data[front] = d;
+            front--;
+            if (front == -1)
+            {
+                front = 999;
+            }
         }
         T pop_front()
         {
-
+            
         }
         void push_back(T d)
         {
-
+            if (size == 1000)
+            {
+                cout << "Deque overflow." << endl;
+                return;
+            }
+            rear++;
+            if (rear == 1000)
+            {
+                rear = 0;
+            }
+            data[rear] = d;
         }
         T pop_back()
         {
-            
+
         }
 };
 
