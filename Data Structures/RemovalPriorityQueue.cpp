082103dbb48dd16249dafd7aa7203fac78134acb
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-08-08 15:38:35 -0400

    added constant-time removeMin() method

--- Data Structures/RemovalPriorityQueue.cpp
+++ Data Structures/RemovalPriorityQueue.cpp
@@ -8,6 +8,17 @@ class PriorityQueue
     private:
         T data[1000];
         int size;
+    public:
+        T removeMin()
+        {
+            if (size == 0)
+            {
+                cout << "Queue underflow." << endl;
+                return T();
+            }
+            T size--;
+            return data[size];
+        }
 };
 
 int main()
