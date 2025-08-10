df5fb055db5860dfd6bdff2b2357020b91318f6a
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-08-10 11:47:25 -0400

    fixed uniquePaths() method with dynamic programming solution

--- Medium/p0062.cpp
+++ Medium/p0062.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int uniquePaths(int n, int m)
+int uniquePaths(int m, int n)
 {
-    if (n == 1 || n == 1)
+    static int memo[100][100] = {0};
+    if (m == 1 || n == 1)
     {
         return 1;
     }
-    return uniquePaths(m-1, n) + uniquePaths(m, n-1);
+    memo[m-1][n] = uniquePaths(m-1, n);
+    memo[m][n-1] = uniquePaths(m, n-1);
+    return memo[m-1][n] + memo[m][n-1];
 }
 
 int main()
 {
-    //code;
+    cout << uniquePaths(7, 3) << endl;
+    cout << uniquePaths(3, 2) << endl;
     return 0;
 }
