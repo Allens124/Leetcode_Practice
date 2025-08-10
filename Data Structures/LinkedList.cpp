e541a9066a7a8476e8beaa977ab0e692a761b62d
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2025-08-10 10:39:14 -0400

    added Node class to the file

---
+++ Data Structures/LinkedList.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+
+class Node
+{
+    private:
+        T data;
+        Node* next;
+    public:
+        Node(T d, Node* n)
+        {
+            data = d;
+            next = n;
+        }
+        void setData(T d)
+        {
+            data = d;
+        }
+        T getData()
+        {
+            return data;
+        }
+        void setNext(Node* n)
+        {
+            next = n;
+        }
+        Node* getNext()
+        {
+            return next;
+        }
+};
+
+int main()
+{
+    //code;
+    return 0;
+}
