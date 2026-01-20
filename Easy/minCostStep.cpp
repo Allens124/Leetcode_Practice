036dd568681e33f90113265cf9b2345339a7eff7
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2026-01-20 11:54:31 -0500

    handled edge case where the size of the cost vector is 1 or less

--- Easy/minCostStep.cpp
+++ Easy/minCostStep.cpp
@@ -7,7 +7,10 @@ int minCostStep(vector<int> cost)
 {
     int size = cost.size();
     int opt[size] = {0};
-    
+    if (size > 1)
+    {
+        opt[1] = min(cost[0], cost[1]);
+    }
 }
 
 int main()
