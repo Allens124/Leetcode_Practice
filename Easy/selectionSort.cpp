67116101ce90b1f1e3075ebe7db83d2b6fcfc4a1
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2026-01-25 17:03:14 -0500

    updated function title line

--- Easy/selectionSort.cpp
+++ Easy/selectionSort.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int selectionSort(int A[], int n)
+void selectionSort(int A[], int n)
 {
-    
+    int minIdx, temp;
+    for (int i = 0; i < n-1; i++)
+    {
+        minIdx = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (A[j] < A[minIdx])
+            {
+                minIdx = j;
+            }
+        }
+        temp = A[i];
+        A[i] = A[minIdx];
+        A[minIdx] = temp;
+    }
 }
 
 int main()
