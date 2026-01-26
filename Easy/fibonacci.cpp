41d6f8ee2aabdbd9cad781123236472640b5682c
Allen Singleton <Allen.Singleton78@qmail.cuny.edu>
2026-01-26 15:17:12 -0500

    created fibonacci() function

--- Easy/fibonacci.cpp
+++ Easy/fibonacci.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-
+int fibonacci(int n)
+{
+    if (n < 2)
+    {
+        return n;
+    }
+    int f0 = 0, f1 = 1, fn;
+    for (int i = 2; i <= n; i++)
+    {
+        fn = f1+f0;
+        f0 = f1;
+        f1 = fn;
+    }
+    return fn;
+}
 
 int main()
 {
